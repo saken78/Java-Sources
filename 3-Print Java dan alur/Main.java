5737279a0169de15c6b0049489ab2e4b0d4cbffc
Saken <sakenspot@gmail.com>
2025-02-19 17:46:44 +0700

    Create Main.java

---
+++ 3-Print Java dan alur/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("hello teman-teman");
+        System.out.println("baris ke-2 : Haloo ini baris ke 2");
+
+        // print taanpa line baru , print tanpa enter lebih mudahnya
+        System.out.print("baris ke-3 : ini adalah baris ketiga");
+        System.out.print("baris ke-4 : saya meneruskan baris ketiga");
+
+        System.out.print("baris ke-5 : haloooooooo");
+        System.out.println("baris ke-6 : Baris baru dengan enter ");
+
+       
+
+
+    }
+}
