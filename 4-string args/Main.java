a970e9031fb5ccca814f111a708a70e2a4ceedcf
Saken <sakenspot@gmail.com>
2025-02-20 19:56:59 +0700

    Create Main.java

---
+++ 4-string args/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("hallo " + args[0] + " Test");
+        System.out.println("hallo " + args[1] + " hiyyahhhh");
+    }
+}
