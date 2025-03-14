c307cb8e95e243d1c4f213ceaea62c98fd3cae16
Saken <sakenspot@gmail.com>
2025-03-14 23:11:41 +0700

    Update Main.java

--- Java OOP/Main.java
+++ Java OOP/Main.java
@@ -27,4 +27,60 @@ class Player{
      this.armor.display();
  }
 }
+//senjata
+class Weapon{
+ double attackPower;
+ String name;
+
+ Weapon(String name, double attackPower){
+     this.name = name;
+     this.attackPower = attackPower;
+ }
+
+ void display(){
+     System.out.println("Weapon : " + this.name + " , attack : " + this.attackPower);
+ }
+}
+
+//armor
+class Armor{
+ double defencePower;
+ String name;
+
+ Armor(String name, double defencePower){
+     this.name = name;
+     this.defencePower = defencePower;
+ }
+
+ void display(){
+     System.out.println("Armor : " + this.name + " , defence : " + this.defencePower);
+ }
+}
+
+public class Main{
+
+ public static void main(String[] args) {
+     
+     // membuat object player
+     Player player1 = new Player("ucup",100);
+     Player player2 = new Player("otong",50);
+
+     // membuat object weapon
+     Weapon pedang = new Weapon("pedang",15);
+     Weapon ketapel = new Weapon("ketapel",1);
+
+     // membuat object armor
+     Armor bajuBesi = new Armor("baju besi",10);
+     Armor kaos = new Armor("kaos",0);
+     
+     // player 1
+     player1.equipWeapon(pedang);
+     player1.equipArmor(bajuBesi);
+     player1.display();
+
+     // player 2
+     player2.equipWeapon(ketapel);
+     player2.equipArmor(kaos);
+     player2.display();
+ }
 
