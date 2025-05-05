70882cdc43e29283f74ce071f7aa23cdeda8f0c2
Saken <sakenspot@gmail.com>
2025-05-05 22:57:16 +0700

    Create unfinished.java

---
+++ hackerRank/unfinished.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class NumberAnalyzer {
+    private int[] numbers;
+
+    // constructor: menerima array
+    public NumberAnalyzer(int[] numbers) {
+        this.numbers = numbers;
+
+    }
+
+    // method: hitung ganjil
+    public int countOdd() { ... }
+
+    // method: hitung genap
+    public int countEven() { ... }
+
+    // method: tampilkan semua angka
+    public void showNumbers() { ... }
+}
+
+public class naz {
+    public static void main(String[] args) {
+        Scanner inputUser = new Scanner(System.in);
+        System.out.println("Masukan berapa angka : ");
+        int index = inputUser.nextInt();
+        int[] numbers = new int[index];
+        int[] buffer;
+        for (int i = 1; i <= index; i++) {
+            System.out.println("masukan angka ke " + i);
+            []buffer = inputUser.nextInt();
+            for (int g = 0; g <= i; g++) {
+                []numbers = buffer;
+            }
+
+        }
+    }
+
+}
