29c0ce4a4dbc86b5197cca36936658273a5cd61a
Saken <sakenspot@gmail.com>
2025-05-06 17:23:27 +0700

    Update unfinished.java

--- hackerRank/unfinished.java
+++ hackerRank/unfinished.java
@@ -3,37 +3,64 @@ import java.util.Scanner;
 class NumberAnalyzer {
     private int[] numbers;
 
-    // constructor: menerima array
     public NumberAnalyzer(int[] numbers) {
         this.numbers = numbers;
 
     }
 
     // method: hitung ganjil
-    public int countOdd() { ... }
+    public void countOdd() {
+        int odd = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 != 0) {
+                odd++;
+            }
+        }
+        System.out.println(odd);
+    }
 
     // method: hitung genap
-    public int countEven() { ... }
+    public void countEven() {
+        int even = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                even++;
+            }
+        }
+        System.out.println(even);
+    }
 
     // method: tampilkan semua angka
-    public void showNumbers() { ... }
+    public void showNumbers() {
+        System.out.println("\nAngka dalam array : ");
+        for (int k = 0; k < numbers.length; k++) {
+            System.out.print(numbers[k] + " ");
+        }
+
+        System.out.print("\njumlah angka ganjil\t: ");
+        countOdd();
+        System.out.print("Jumlah angka genap\t: ");
+        countEven();
+
+    }
 }
 
 public class naz {
     public static void main(String[] args) {
         Scanner inputUser = new Scanner(System.in);
-        System.out.println("Masukan berapa angka : ");
+        System.out.print("Masukan berapa angka\t: ");
         int index = inputUser.nextInt();
         int[] numbers = new int[index];
-        int[] buffer;
-        for (int i = 1; i <= index; i++) {
-            System.out.println("masukan angka ke " + i);
-            []buffer = inputUser.nextInt();
-            for (int g = 0; g <= i; g++) {
-                []numbers = buffer;
-            }
+
+        for (int i = 0; i < index; i++) {
+            System.out.print("masukan angka ke-" + (i + 1) + " : ");
+            numbers[i] = inputUser.nextInt();
 
         }
+
+        NumberAnalyzer ar1 = new NumberAnalyzer(numbers);
+        ar1.showNumbers();
+
     }
 
 }
