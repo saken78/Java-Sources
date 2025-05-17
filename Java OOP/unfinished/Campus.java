8ef0d145d3621d599e5bcb07bb15e06bb55bfb39
sakenx <sakenspot@gmail.com>
2025-05-17 10:34:51 +0700

    Update Campus.java

--- Java OOP/unfinished/Campus.java
+++ Java OOP/unfinished/Campus.java
@@ -4,15 +4,20 @@ import java.util.ArrayList;
 
 public class Campus {
     public ArrayList<String> nameSended = new ArrayList<>();
+    public ArrayList<Double> sender = new ArrayList<>();
 
-    Campus(ArrayList<String> nama) {
+    Campus(ArrayList<String> nama, ArrayList<Double> sended) {
         this.nameSended = nama;
+        this.sender = sended;
     }
 
     public void show() {
         System.out.println("Data Statistik");
         for (String n : nameSended) {
-            System.out.println("Nama : " + n);
+            for (Double m : sender) {
+                System.out.println("Nama : " + n);
+                System.out.println("Average : " + m);
+            }
         }
 
     }
