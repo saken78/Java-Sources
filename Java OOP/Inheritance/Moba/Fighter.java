639185a650b3dfe4f823160fca130ffc803101d6
sakenx <sakenspot@gmail.com>
2025-05-24 20:02:40 +0700

    Create Fighter.java

---
+++ Java OOP/Inheritance/Moba/Fighter.java
@@ -0,0 +1,28 @@
+package inheritance.Moba;
+
+public class Fighter extends Hero {
+    private String type = "Fighter";
+    private double physicalDefence = 20;
+
+    Fighter(String namaInput, double healthInput, double basicAttackInput) {
+        super(namaInput, healthInput, basicAttackInput);
+    }
+
+    @Override
+    void show() {
+        System.out.println("\nNama\t: " + this.nama);
+        System.out.println("Health\t: " + this.health);
+        System.out.println("Defence\t: " + this.physicalDefence);
+        System.out.println("Type\t: " + this.type);
+    }
+
+    @Override
+    void takeDamage(double basicAttack) {
+        double defence = basicAttack - this.physicalDefence;
+        this.health -= defence;
+
+    }
+
+}
+
+
