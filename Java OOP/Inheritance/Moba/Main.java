395f7f84d1dc9c5d40fc3724cd1d5700188d9091
sakenx <sakenspot@gmail.com>
2025-05-25 23:08:53 +0700

    Update Main.java

--- Java OOP/Inheritance/Moba/Main.java
+++ Java OOP/Inheritance/Moba/Main.java
@@ -4,16 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Assasin hero1 = new Assasin("Ling", 1000, 90);
 
-        Mage hero2 = new Mage("Lunox", 1000, 30);
+        Mage hero2 = new Mage("Lunox", 1000, 70);
 
-        Tanker hero3 = new Tanker("Atlas", 2000, 10);
+        Tanker hero3 = new Tanker("Atlas", 2000, 40);
 
         Fighter hero4 = new Fighter("Thamuz", 1200, 40);
-        hero1.levelUp();
-        hero2.show();
+
+        hero3.specialAbillity();
+        hero2.specialAbillity();
+        hero2.attack(hero3);
         hero3.show();
-        hero1.show();
-        hero4.show();
+        // hero2.attack(hero3);
+
+        // next debuff
+
     }
 }
+
 
