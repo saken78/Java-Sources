a10f705e61d907787629e3a340461b83d8855540
sakenx <sakenspot@gmail.com>
2025-05-25 16:39:12 +0700

    Update Main.java

--- Java OOP/Inheritance/Moba/Main.java
+++ Java OOP/Inheritance/Moba/Main.java
@@ -3,22 +3,17 @@ package inheritance.Moba;
 public class Main {
     public static void main(String[] args) {
         Assasin hero1 = new Assasin("Ling", 1000, 90);
-        hero1.show();
 
         Mage hero2 = new Mage("Lunox", 1000, 30);
-        hero2.show();
 
         Tanker hero3 = new Tanker("Atlas", 2000, 10);
-        hero3.show();
 
         Fighter hero4 = new Fighter("Thamuz", 1200, 40);
-        hero4.show();
-
-        hero1.attack(hero3);
-
+        hero1.levelUp();
+        hero2.show();
         hero3.show();
-
+        hero1.show();
+        hero4.show();
     }
 }
-
 
