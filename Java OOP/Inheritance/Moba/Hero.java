68dc708448f488536e141d46d606c3536ec9354d
sakenx <sakenspot@gmail.com>
2025-05-25 16:40:29 +0700

    Update Hero.java

--- Java OOP/Inheritance/Moba/Hero.java
+++ Java OOP/Inheritance/Moba/Hero.java
@@ -1,15 +1,72 @@
 package inheritance.Moba;
 
 public abstract class Hero {
-    String nama;
-    double health;
-    double basicAttack;
 
+    // atribut superclass
+    private String nama;
+    private double health;
+    private double basicAttack;
+    private int level;
+
+    // constructor superclass
     Hero(String namaInput, double healthInput, double basicAttackInput) {
         this.nama = namaInput;
         this.health = healthInput;
         this.basicAttack = basicAttackInput;
+        this.level = 1;
     }
+
+    // method superclass
+    public String getNama() {
+        return this.nama;
+    }
+
+    public double getHealth() {
+        return this.health;
+    }
+
+    public double getBasicAttack() {
+        return this.basicAttack;
+    }
+
+    public int getLevel() {
+        return this.level;
+    }
+
+    // decreasing health
+    public void setHealthAttack(double bufferHealth) {
+        this.health -= bufferHealth;
+    }
+
+    // increasing health
+    public void setHealthIncrease(double bufferHealth) {
+        this.health += bufferHealth;
+    }
+
+    // set new name
+    public void setNama(String newNama) {
+        this.nama = newNama;
+    }
+
+    // increasing basic attack
+    public void setIncreaseBasicAttack(double bufferBasicAttack) {
+        this.basicAttack += bufferBasicAttack;
+    }
+
+    // decreasing basic attack
+    public void setDecreaseBasicAttack(double bufferBasicAttack) {
+        this.basicAttack -= bufferBasicAttack;
+    }
+
+    // abstract class
+    public abstract void levelUp();
+
+    // method levelling
+    public void setLevelUp(int bufferLevel) {
+        this.level += bufferLevel;
+    }
+
+    // common method
 
     protected void attack(Hero enemy) {
         System.out.println("\n" + this.nama + " attacking " + enemy.nama);
@@ -21,11 +78,11 @@ public abstract class Hero {
     }
 
     protected void show() {
-        System.out.println("\nNama : " + this.nama);
-        System.out.println("Health : " + this.health);
-        System.out.println("Attack Power : " + this.basicAttack);
+        System.out.println("\nNama\t: " + this.nama);
+        System.out.println("Health\t: " + this.health);
+        System.out.println("Level\t: " + this.level);
+
     }
 
 }
-
 
