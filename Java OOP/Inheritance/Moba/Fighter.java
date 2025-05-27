fe61abf45fb9f51beaff48e618f815b8277ab27f
sakenx <sakenspot@gmail.com>
2025-05-27 23:04:13 +0700

    Update Fighter.java

--- Java OOP/Inheritance/Moba/Fighter.java
+++ Java OOP/Inheritance/Moba/Fighter.java
@@ -1,6 +1,6 @@
 package inheritance.Moba;
 
-public class Fighter extends Hero {
+public class Fighter extends Hero implements IAttack, IBuff {
     private String type = "Fighter";
     private double physicalDefence = 20;
 
@@ -8,19 +8,43 @@ public class Fighter extends Hero {
         super(namaInput, healthInput, basicAttackInput);
     }
 
-    @Override
+    @Override // interface
+    public void buff() {
+        this.setIncreasePhysicalDefence(70);
+        System.out.println("\n" + this.getNama() + " mendapatkan buff physichal defense " + this.getPhysicalDefence());
+    }
+
+    @Override // interface
+    public void attack(Hero enemy) {
+        System.out.println("\n" + this.getNama() + " attacking " + enemy.getNama());
+        enemy.takeDamage(this.getBasicAttack());
+    }
+
+    @Override // method hero
     protected void show() {
         super.show();
         System.out.println("Defence\t: " + this.physicalDefence);
         System.out.println("Type\t: " + this.type);
     }
 
-    @Override
+    @Override // method hero
     protected void takeDamage(double basicAttack) {
         double defence = basicAttack - this.physicalDefence;
         System.out.println(this.getNama() + " menerima " + defence);
         this.setHealthAttack(defence);
 
+    }
+
+    public void takeDamagedPhysic(double basicAttack) {
+        double defence = basicAttack - this.physicalDefence;
+        System.out.println(this.getNama() + " menerima " + defence);
+        this.setHealthAttack(defence);
+    }
+
+    public void takeDamagedMagic(double magicPowerInput) {
+        double defence = magicPowerInput - this.physicalDefence;
+        System.out.println(this.getNama() + " menerima " + defence);
+        this.setHealthAttack(defence);
     }
 
     // override method abstract
@@ -29,13 +53,4 @@ public class Fighter extends Hero {
         this.setLevelUp(1);
     }
 
-    // ovveride method abstract
-    @Override
-    public void specialAbillity() {
-        this.physicalDefence += 20;
-        System.out.println("\n" + this.getNama() + " menerima buff defence " + this.physicalDefence);
-    }
-
 }
-
-
