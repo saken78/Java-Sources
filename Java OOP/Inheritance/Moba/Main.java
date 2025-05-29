1c62c9b725b847b93fc390af21725f0293cbeba0
sakenx <sakenspot@gmail.com>
2025-05-29 21:52:50 +0700

    Update Main.java

--- Java OOP/Inheritance/Moba/Main.java
+++ Java OOP/Inheritance/Moba/Main.java
@@ -8,12 +8,13 @@ public class Main {
         Assasin hero1 = new Assasin("Ling", 1000, 90);
 
         Mage hero2 = new Mage("Lunox", 1000, 70);
+        Mage hero5 = new Mage("Alice", 1000, 120);
 
         Tanker hero3 = new Tanker("Atlas", 2000, 40);
 
         Fighter hero4 = new Fighter("Thamuz", 1200, 40);
 
-        Mage hero5 = new Mage("Alice", 1000, 120);
+        Healer hero6 = new Healer("Rafaela", 700, 30);
 
         ArrayList<Hero> kumpulanHero = new ArrayList<Hero>();
 
@@ -22,18 +23,16 @@ public class Main {
         kumpulanHero.add(hero3);
         kumpulanHero.add(hero4);
         kumpulanHero.add(hero5);
+        kumpulanHero.add(hero6);
 
         for (Hero hero : kumpulanHero) {
             hero.show();
         }
 
-        hero1.buff();
+        hero6.buff();
+        hero2.attack(hero6);
 
-        hero1.attack(hero5);
-
-        hero5.show();
+        hero6.show();
 
     }
 }
-
-
