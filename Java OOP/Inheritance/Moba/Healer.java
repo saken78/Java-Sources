9fd54abee6d2d3e3cbdc9393e670253e71be30d9
sakenx <sakenspot@gmail.com>
2025-05-29 21:53:31 +0700

    Create Healer.java

---
+++ Java OOP/Inheritance/Moba/Healer.java
@@ -0,0 +1,46 @@
+package inheritance.Moba;
+
+public class Healer extends Hero implements IBuff, IDebuff {
+    private String type = "Healer";
+
+    Healer(String namaInput, double healthInput, double magicPowerInput) {
+        super(namaInput, healthInput, magicPowerInput);
+    }
+
+    @Override // interface
+    public void buff() {
+        this.setHealthIncrease(12);
+        System.out.println("\n" + this.getNama() + " mendapatkan buff health " + this.getHealth());
+    }
+
+    @Override // interface
+    public void debuff() {
+        this.setHealthIncrease(23);
+        System.out.println("\n" + this.getNama() + " mendapatat debuff " + this.getHealth());
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        System.out.println("Type\t: " + this.type);
+    }
+
+    public void takeDamagedMagic(double magicPowerInput) {
+        // double defence = magicPowerInput;
+        // karena type mage tidak memiliki defence
+        System.out.println(this.getNama() + " menerima " + magicPowerInput + " damage");
+        this.setHealthAttack(magicPowerInput);
+    }
+
+    @Override
+    public void takeDamagedPhysic(double magicPower) {
+        double defence = magicPower;
+        System.out.println(this.getNama() + " menerima " + defence);
+        this.setHealthAttack(defence);
+    }
+
+    @Override
+    public void levelUp() {
+        this.setLevelUp(1);
+    }
+}
