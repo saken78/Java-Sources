242837b49036504331e56338f6bb54dac1e52561
sakenx <sakenspot@gmail.com>
2025-05-30 13:59:13 +0700

    Update Healer.java

--- Java OOP/Inheritance/Moba/Healer.java
+++ Java OOP/Inheritance/Moba/Healer.java
@@ -1,10 +1,16 @@
 package inheritance.Moba;
 
-public class Healer extends Hero implements IBuff, IDebuff {
+public class Healer extends Hero implements IBuff, IDebuff, IRegen {
     private String type = "Healer";
 
     Healer(String namaInput, double healthInput, double magicPowerInput) {
         super(namaInput, healthInput, magicPowerInput);
+    }
+
+    @Override
+    public void regen() {
+        this.setHealthIncrease(this.getHealth());
+        System.out.println("\nHealer bisa heal HP 100% ++");
     }
 
     @Override // interface
@@ -42,5 +48,7 @@ public class Healer extends Hero implements IBuff, IDebuff {
     @Override
     public void levelUp() {
         this.setLevelUp(1);
+        this.setHealthIncrease(100);
+
     }
 }
