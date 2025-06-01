47ef09061bcea4f078c8a8f950e19b10120fa4ed
sakenx <sakenspot@gmail.com>
2025-06-01 22:35:48 +0700

    Create ITakeDamaged.java

---
+++ Java OOP/Inheritance/MobaV2/ITakeDamaged.java
@@ -0,0 +1,9 @@
+//package inheritance.Moba;
+
+public interface ITakeDamaged {
+
+    public void takeDamagedMagic(double magicPowerInput);
+
+    public void takeDamagedPhysic(double physicalDamageInput);
+
+}
